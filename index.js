1a8b917951211176f7b1bb3d38b5a254ec586111
Mudit Jain <muditjain.1406@gmail.com>
2026-02-08 00:20:51 +0530

    saveTasks and generateTaskId function created

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ import os from 'os'
 const TASK_FILE_PATH = path.join(os.homedir(), '.taskify-data.json')
 const data = loadTasks()
 
+function generateTaskId(category) {
+
+    let id = `${category}@${String(Math.floor(Math.random() * 10000)).padStart(4, '0')}`
+    while (id in data[category]) {
+        id = `${category}@${String(Math.floor(Math.random() * 10000)).padStart(4, '0')}`
+    }
+    return id
+
+}
 function loadTasks() {
     let data;
     if (fs.existsSync(TASK_FILE_PATH)) {
@@ -18,4 +27,21 @@ function loadTasks() {
     }
 
     return JSON.parse(data)
+}
+
+function saveTasks(tasks, category = 'general') {
+    category = category.toLowerCase()
+
+    if (!data[category]) {
+        console.log(`Category ${category} does not exist.\nTerminating addition of task.`)
+        return
+    }
+
+    const id = generateTaskId(category)
+    data[category][id] = {}
+    data[category][id]["task"] = tasks
+    data[category][id]["status"] = "pending"
+    console.log(data)
+    fs.writeFileSync(TASK_FILE_PATH, JSON.stringify(data))
+    console.log(`Task added with ID: ${id}`)
 }
