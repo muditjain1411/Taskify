500260f593b759129ab0e127ed4164c84d0e9fe9
Mudit Jain <muditjain.1406@gmail.com>
2026-02-08 00:18:02 +0530

    loadTasks() function created

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+const TASK_FILE_PATH = path.join(os.homedir(), '.taskify-data.json')
+const data = loadTasks()
+
+function loadTasks() {
+    let data;
+    if (fs.existsSync(TASK_FILE_PATH)) {
+        data = fs.readFileSync(TASK_FILE_PATH, 'utf-8')
+    }
+    else {
+        data = '{"general":{}}\n'
+        fs.writeFileSync(TASK_FILE_PATH, data)
+    }
+
+    return JSON.parse(data)
+}
